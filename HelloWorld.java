9540ca00c3727d24f497ca466aea4455d3b34979
Pedro-Armando-M <04157326@sempreunama.com.br>
2024-08-28 12:35:26 +0000

    Aula02

---
+++ HelloWorld.java
@@ -0,0 +1,36 @@
+//aula dia 28 de agosto
+
+class HelloWorld{
+    public static void main(String[] args) {
+        
+        int idade;
+        idade = 14;
+        
+        int idade_ano_que_vem = idade + 1;
+
+        double metadeidade = idade / 2;
+        double resultado = idade % 2;
+
+        char tiposangue = 'O';
+
+        boolean maioridade;
+        maioridade = idade >= 18;
+        
+        System.out.println(idade);
+        System.out.println(idade_ano_que_vem);
+        System.out.println(metadeidade);
+        System.out.println(resultado);
+        System.out.println(tiposangue);
+        System.out.println(maioridade);
+
+        int i = 5;
+        int j = i;
+        System.out.println(i);
+        System.out.println(j);
+        
+        i= i + 1
+        System.out.println(i);
+       
+
+    }
+}
